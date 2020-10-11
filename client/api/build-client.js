ad091a37f28a1040faf575e83203050338e3548e
alcaamoti <mikyd3000@gmail.com>
2020-10-11 11:12:05 -0400

    bug fixes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ const buildClient = ({ req }) => {
     if(typeof window === 'undefined') {
         //onserver
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local', 
+            baseURL: 'http://www.bigcouch.app', 
             headers: req.headers
         });
     } else {
